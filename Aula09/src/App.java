546cd107618b048a2e868a904c0432f6df6749d7
MatheusKudlake <mkudlake@gmail.com>
2025-04-04 21:36:04 -0300

    menu

--- Aula09/src/App.java
+++ Aula09/src/App.java
@@ -1,24 +1,19 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class App {
     public static void main(String[] args) {
+        
+        ArrayList<Animal> listaAnimais = new ArrayList<>();
+
         //--Animal--
-        Animal cachorro = new Animal();
+        listaAnimais.add(Menu.cadastrarAnimal());
         Scanner input = new Scanner(System.in);
-        
-        System.out.println("Digite o nome do animal:");
-        cachorro.setNome(input.nextLine());
-        
-        System.out.println("Digite o peso do animal:");
-        cachorro.setPeso(input.nextFloat());
-        
-        System.out.println("Digite o recinto do animal:");
-        cachorro.setRecinto(input.nextInt());
-        
-        System.out.println(cachorro.toString());
         
         //--Ave--
         Ave papagaio = new Ave();
+
+        input.nextLine();
         
         System.out.println("Digite o nome da ave:");
         papagaio.setNome(input.nextLine());
@@ -29,18 +24,24 @@ public class App {
         System.out.println("Digite o recinto da ave:");
         papagaio.setRecinto(input.nextInt());
 
+        input.nextLine();
+
         System.out.println("Digite o local nativo da ave:");
         papagaio.setLocal(input.nextLine());
         
         System.out.println("A ave faz migração? Digite sim ou não.");
+        
         if(input.nextLine().equalsIgnoreCase("sim")){
             papagaio.setFazMigracao(true);
+
         }else if(input.nextLine().equalsIgnoreCase("não")){
             papagaio.setFazMigracao(false);
+
         }else{
             System.out.println("Valor inválido. Atribuindo valor falso à variável.");
             papagaio.setFazMigracao(false);
         }        
+
         System.out.println(papagaio.toString());
         input.close();
     }
--- Aula09/src/Menu.java
+++ Aula09/src/Menu.java
@@ -3,7 +3,16 @@ import java.util.Scanner;
 public class Menu {
     public static Animal cadastrarAnimal(){
         Scanner input = new Scanner(System.in);
-        Animal animal1 = new Animal();
+        int entrada;
+        do{
+            System.out.println("Selecione um animal para cadastrar:");
+            System.out.println("1. Animal");
+            System.out.println("2. Ave");
+            System.out.println("3. Sair");
+            System.out.println("Insira a opção desejada:");
+            entrada = input.nextInt();
+        }while(entrada != 3);
+        Ave animal1 = new Ave();
 
         System.out.println("Digite o nome do animal:");
         animal1.setNome(input.nextLine());
